ebe567a5129fb4bed3c6ca072776949b809221cc
ducnmcoder <duc29081234@gmail.com>
2026-07-26 20:37:44 +0700

    Final fix for Render: War packaging, clean props, and PostgreSQL TEXT types

--- java-spring-laptopshop-final-project-v2/src/main/java/laptopshop/domain/Blog.java
+++ java-spring-laptopshop-final-project-v2/src/main/java/laptopshop/domain/Blog.java
@@ -30,7 +30,7 @@ public class Blog implements Serializable {
 
     @NotNull
     @NotEmpty(message = "Content cannot be empty")
-    @Column(columnDefinition = "MEDIUMTEXT")
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     private String image;
